1f4261c5c09bf449f1f4de0f101163179f530b19
nataliafig <71331067+nataliafig@users.noreply.github.com>
2020-09-18 10:05:38 -0300

    Add files via upload
    Programming Assignment 2: Lexical Scoping - Week 2

---
+++ cachematrix.R
@@ -0,0 +1,21 @@
+################################################################################
+######################### R Programming- Week 3#################################
+################################################################################
+
+##Functions to cache the inverse of a matrix
+
+## Creating a special "matrix" object that can cache its inverse
+
+makeCacheMatrix <- function(x = matrix()) {
+
+## starting the inverse property
+    ip <-  null    
+
+    }
+
+
+## Write a short comment describing this function
+
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
+}
